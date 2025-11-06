8b4f6cd82b9e4a6e9ae622b4a92a20eed7477c74
perjaaaan <benjaminjavier46@gmail.com>
2025-11-05 21:07:29 -0300

    TOKENS

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -25,6 +25,10 @@ SECRET_KEY = 'django-insecure-j)$i8#vx-^gdg#apoumb9gkb*u6b)=viy(rcm&1=jb@(f+e$l8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['proud-integrity-production.up.railway.app'] 
+CSRF_TRUSTED_ORIGINS = [
+    'https://proud-integrity-production.up.railway.app',
+]
+
 
 
 
