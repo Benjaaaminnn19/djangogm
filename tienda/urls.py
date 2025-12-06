27d62e059cdcc774112a52bcd8ca09f64f7be0e3
perjaaaan <benjaminjavier46@gmail.com>
2025-12-06 07:26:07 -0300

    nose

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.tienda, name='tienda'),
     path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
-    path('test/', views.tienda_test, name='tienda_test'),
-    path('simple/', views.tienda_simple, name='tienda_simple'),
-    path('raw/', views.tienda_raw, name='tienda_raw'),
+
 ]
