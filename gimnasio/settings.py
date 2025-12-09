d173b31f6ed17c9d7cef01ceab2540d5358a2487
perjaaaan <benjaminjavier46@gmail.com>
2025-12-08 23:07:59 -0300

    arreglo

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -24,12 +24,17 @@ SECRET_KEY = 'django-insecure-j)$i8#vx-^gdg#apoumb9gkb*u6b)=viy(rcm&1=jb@(f+e$l8
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['proud-integrity-production.up.railway.app', 'gimnasiolebloncalama.cl']
-CSRF_TRUSTED_ORIGINS = [
-    "https://proud-integrity-production.up.railway.app",
-    "https://www.gimnasiolebloncalama.cl"
+ALLOWED_HOSTS = [
+    "proud-integrity-production.up.railway.app",
+    "gimnasiolebloncalama.cl",
+    "www.gimnasiolebloncalama.cl",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://proud-integrity-production.up.railway.app",
+    "https://gimnasiolebloncalama.cl",
+    "https://www.gimnasiolebloncalama.cl",
+]
 
 
 
