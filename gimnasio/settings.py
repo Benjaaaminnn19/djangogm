ff5a3aea699e5e53c7f247cf58afc52ede312458
perjaaaan <benjaminjavier46@gmail.com>
2025-12-12 07:10:02 -0300

    admin

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j)$i8#vx-^gdg#apoumb9gkb*u6b)=viy(rcm&1=jb@(f+e$l8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     "proud-integrity-production.up.railway.app",
     "gimnasiolebloncalama.cl",
