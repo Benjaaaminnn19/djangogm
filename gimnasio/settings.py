060370207fffefdf89d9e3b4d1ef7a82bdda3727
perjaaaan <benjaminjavier46@gmail.com>
2026-03-13 20:31:39 -0300

    Base de datos configurada

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from decouple import config, Csv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,10 +86,10 @@ WSGI_APPLICATION = 'gimnasio.wsgi.application'
 # ==========================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"),
+        conn_max_age=600
+    )
 }
 
 
